46ba526fde812aca32b5d4b4f7896be16499577c
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2025-05-26 08:29:57 +0500

    gcc build fix

--- src/Game/GameObjects/Grid.hpp
+++ src/Game/GameObjects/Grid.hpp
@@ -10,17 +10,17 @@ public:
 	Grid() : m_gridSize(), m_cellSize(), m_offset() {}
 
 	void init(
-		std::pair<size_t, size_t> gridSize = {0, 0},
-		std::pair<size_t, size_t> cellSize = {0, 0},
+		std::pair<std::size_t, std::size_t> gridSize = {0, 0},
+		std::pair<std::size_t, std::size_t> cellSize = {0, 0},
 		std::pair<int, int> offset = {0, 0});
 	void update();
 	void render(SDL_Renderer* renderer);
 
 	bool isWallCollision(const std::pair<int, int>& position) const;
-	const std::pair<size_t, size_t>& getGridSize() const { return m_gridSize; }
+	const std::pair<std::size_t, std::size_t>& getGridSize() const { return m_gridSize; }
 
 private:
-	std::pair<size_t, size_t> m_gridSize{0,0};
-	std::pair<size_t, size_t> m_cellSize{0,0};
+	std::pair<std::size_t, std::size_t> m_gridSize{0,0};
+	std::pair<std::size_t, std::size_t> m_cellSize{0,0};
 	std::pair<int, int> m_offset{0,0};
 };
